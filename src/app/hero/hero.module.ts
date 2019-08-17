6495434e5f557e060ac2690d4969e6e337647514
CloverFeng <sunkesi02@gmail.com>
2019-08-17 23:11:12 +0800

    Mision The Hero Editor Complete

--- src/app/hero/hero.module.ts
+++ src/app/hero/hero.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HeroRoutingModule } from './hero-routing.module';
-import { HerosComponent } from './heros/heros.component';
+import { HeroRoutingModule } from "./hero-routing.module";
+import { HerosComponent } from "./heros/heros.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [HerosComponent],
   imports: [
     CommonModule,
-    HeroRoutingModule
+    HeroRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
-export class HeroModule { }
+export class HeroModule {}
--- src/app/hero/heros/heros.component.ts
+++ src/app/hero/heros/heros.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import { Hero } from "../../models/hero";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-heros',
-  templateUrl: './heros.component.html',
-  styleUrls: ['./heros.component.css']
+  selector: "app-heros",
+  templateUrl: "./heros.component.html",
+  styleUrls: ["./heros.component.css"]
 })
-export class HerosComponent implements OnInit {
-
-  constructor() { }
+export class HerosComponent implements OnInit, AfterViewInit {
+  form: FormGroup;
+  hero: Hero = {
+    id: 1,
+    name: "Windstorm"
+  };
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.form = this.fb.group({
+      heroName: [""]
+    });
   }
 
+  ngAfterViewInit(): void {
+    this.form.get("heroName").valueChanges.subscribe(val => {
+      this.hero.name = val;
+    });
+  }
 }
