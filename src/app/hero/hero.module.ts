5c3b2fab56887593874aa94f893673d5e1f83642
CloverFeng <sunkesi02@gmail.com>
2019-08-17 22:44:22 +0800

    Create Hero Module

---
+++ src/app/hero/hero.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeroRoutingModule } from './hero-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HeroRoutingModule
+  ]
+})
+export class HeroModule { }
