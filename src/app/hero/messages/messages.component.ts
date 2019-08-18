66ad7073eefc767e374020f5cf0c89e9f6636ed9
CloverFeng <sunkesi02@gmail.com>
2019-08-18 11:32:01 +0800

    Mission4 Services Complete

--- src/app/hero/messages/messages.component.ts
+++ src/app/hero/messages/messages.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MessageService } from "../../services/message.service";
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  selector: "app-messages",
+  templateUrl: "./messages.component.html",
+  styleUrls: ["./messages.component.css"]
 })
 export class MessagesComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private messageService: MessageService) {}
+  messages: string[] = [];
   ngOnInit() {
+    this.messages = this.messageService.messages;
   }
 
+  clear() {
+    this.messageService.clear();
+  }
 }
