d80cb68cd98fd58bc3581590e66f67ebc7f029d4
CloverFeng <sunkesi02@gmail.com>
2019-08-18 14:49:00 +0800

    Mission6 Install json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { MessagesComponent } from './messages/messages.component';
+import { MessagesComponent } from "./messages/messages.component";
 
 @NgModule({
   declarations: [AppComponent, MessagesComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [{ provide: "api", useValue: "http://localhost:3000/api" }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/hero/hero.module.ts
+++ src/app/hero/hero.module.ts
@@ -9,6 +9,18 @@ import { FormsModule } from "@angular/forms";
 import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 
+// Mission6 Http 更改使用方法
+// 安裝 JSON Server
+// npm install -g json-server
+// mkdir json-server --建立資料夾
+// cd json-server  -- 進入資料夾
+// 建立 db.json 檔案
+// 將 hero 資料寫入
+// 建立 routes.json 檔案
+// 寫入 API 路徑
+// app.module 內的 加入 provider 並加入 { provide: "api", useValue: "http://localhost:3000/api" }
+// 在 json-server 內啟動，指令 ->  json-server --watch db.json --routes routes.json
+
 @NgModule({
   declarations: [HerosComponent, HeroDetailComponent, DashboardComponent],
   imports: [
