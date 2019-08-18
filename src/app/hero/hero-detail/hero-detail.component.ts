3cbf450328e05fa55b3d524b4b5d98e1accfd2c8
CloverFeng <sunkesi02@gmail.com>
2019-08-18 10:58:55 +0800

    Mission3 Master/Detail Components Complete

--- src/app/hero/hero-detail/hero-detail.component.ts
+++ src/app/hero/hero-detail/hero-detail.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Hero } from "./../../models/hero";
+import { FormGroup } from "@angular/forms";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  selector: "app-hero-detail",
+  templateUrl: "./hero-detail.component.html",
+  styleUrls: ["./hero-detail.component.css"]
 })
 export class HeroDetailComponent implements OnInit {
+  @Input() form: FormGroup;
+  @Input() hero: Hero;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
