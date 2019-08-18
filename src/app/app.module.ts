e5b1259d7e50cbfcaf24ae9c36f3bcddd9fe3216
CloverFeng <sunkesi02@gmail.com>
2019-08-18 10:51:12 +0800

    fix error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HeroDetailComponent } from './heros/hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroDetailComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
